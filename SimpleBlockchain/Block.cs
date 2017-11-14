10237c586cf273699758db5ea1722efaa660a56a
AshV <ashish6318@gmail.com>
2017-11-14 08:17:17 +0530

    Added Block class]

---
+++ SimpleBlockchain/Block.cs
@@ -0,0 +1,20 @@
+﻿namespace SimpleBlockchain
+{
+    class Block
+    {
+        private int previousHash { get; }
+
+        public string[] transactions { get; }
+
+        private int blockhash { get; }
+
+        public Block(int previousHash, string[] transactions)
+        {
+            this.previousHash = previousHash;
+            this.transactions = transactions;
+
+            object[] contents = { transactions, GetHashCode(), previousHash };
+            this.blockhash = contents.GetHashCode();
+        }
+    }
+}
--- SimpleBlockchain/Program.cs
+++ SimpleBlockchain/Program.cs
@@ -10,6 +10,7 @@ namespace SimpleBlockchain
     {
         static void Main(string[] args)
         {
+            
         }
     }
 }
